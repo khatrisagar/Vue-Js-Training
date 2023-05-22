8a54d7e0ac505f9f68de6a66370ca5fc9725eb4d
khatrisagar <khatrisagar404@gmail.com>
2023-05-22 19:49:59 +0530

    set a default api call and header

--- Vue Js Tasks/8-big-sell/src/main.js
+++ Vue Js Tasks/8-big-sell/src/main.js
@@ -3,6 +3,18 @@ import App from "./App.vue";
 import "@/styles/style.css";
 import { router } from "@/routes";
 
+
+import axios from "axios";
+
+axios.interceptors.request.use((config) => {
+    const big_sell_at = localStorage.getItem('big_sell_at') ?? 'aaa';
+    config.baseURL = "http://localhost:3000"
+    config.headers['Authorization'] = `Bearer ${big_sell_at}`
+    return config
+})
+
+
 const app = createApp(App);
+
 app.use(router);
 app.mount("#app");
