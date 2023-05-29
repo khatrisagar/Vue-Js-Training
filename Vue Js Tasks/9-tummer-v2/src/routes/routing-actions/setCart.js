8d002e99b8878f5fb9395ebf02db8856b8ff5d7a
khatrisagar <khatrisagar404@gmail.com>
2023-05-29 09:46:20 +0530

    default setters added on first time using

--- Vue Js Tasks/9-tummer-v2/src/routes/routing-actions/setCart.js
+++ Vue Js Tasks/9-tummer-v2/src/routes/routing-actions/setCart.js
@@ -1,5 +1,26 @@
 import { store } from "@/store/store";
+// import {
+//     productCategories,
+//     productSubCategories,
+//     products,
+// } from "@/constants/products";
+
+// import { users } from "@/constants/users";
+
+// export const setAllItems = () => {
+//     localStorage.setItem(
+//         "productCategories",
+//         JSON.stringify(productCategories)
+//     );
+//     localStorage.setItem("products", JSON.stringify(products));
+//     localStorage.setItem(
+//         "productSubCategories",
+//         JSON.stringify(productSubCategories)
+//     );
+//     localStorage.setItem("users", JSON.stringify(users));
+// };
 
 export const setCart = () => {
+    // setAllItems();
     store.dispatch("loadCart");
 };
--- Vue Js Tasks/9-tummer-v2/src/utils/helpers/getCategories.js
+++ Vue Js Tasks/9-tummer-v2/src/utils/helpers/getCategories.js
@@ -1,9 +1,10 @@
-import { productCategories } from "@/constants/products";
-const setProductCategories = () => {
-  localStorage.setItem("productCategories", JSON.stringify(productCategories));
-};
+// import { productCategories } from "@/constants/products";
+// const setProductCategories = () => {
+//   localStorage.setItem("productCategories", JSON.stringify(productCategories));
+// };
 
 export const getCategories = () => {
-  setProductCategories();
-  return JSON.parse(localStorage.getItem("productCategories"));
+    // setProductCategories();
+
+    return JSON.parse(localStorage.getItem("productCategories"));
 };
