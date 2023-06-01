4ee6b977d13495c68fab188902132e957d89108d
khatrisagar <khatrisagar404@gmail.com>
2023-06-01 21:48:36 +0530

    small bug fixed

--- Vue Js Tasks/13-todo-app/src/pages/post/view-single-post.page.vue
+++ Vue Js Tasks/13-todo-app/src/pages/post/view-single-post.page.vue
@@ -1,18 +1,17 @@
 <template>
-    <h1>This is the single post</h1>
-    <div v-if="!isLoader">
-        <commonCard :post="getCurrentPost.postData" />
+  <h1>This is the single post</h1>
+  <div v-if="!isLoader">
+    <commonCard :post="getCurrentPost.postData" />
 
-        <div>
-            <p>comments</p>
-            <commentsCard
-                v-for="comment in getCurrentPost.commnetsData"
-                :key="comment.id"
-                :comment="comment"
-            />
-        </div>
-        {{ getCurrentPost }}
+    <div>
+      <p>comments</p>
+      <commentsCard
+        v-for="comment in getCurrentPost.commnetsData"
+        :key="comment.id"
+        :comment="comment"
+      />
     </div>
+  </div>
 </template>
 
 <script>
@@ -24,35 +23,41 @@ import commonCard from "@/components/common/common-post-card.component.vue";
 import { mapActions, mapGetters } from "vuex";
 
 export default {
-    components: {
-        commentsCard,
-        commonCard,
-    },
-    async created() {
-        try {
-            const postInfo = await Promise.all([
-                getPost(this.$route.params.postId),
-                getPostComments(this.$route.params.postId),
-            ]);
-            this.setCurrentPost({
-                postData: postInfo[0].data,
-                commnetsData: postInfo[1].data,
-            });
-            this.isLoader = false;
-        } catch (error) {
-            this.$router.push({ name: "404" });
-        }
-    },
-    data() {
-        return {
-            isLoader: true,
-        };
-    },
-    methods: {
-        ...mapActions({ setCurrentPost: "setCurrentPost" }),
-    },
-    computed: {
-        ...mapGetters({ getCurrentPost: "getCurrentPost" }),
-    },
+  components: {
+    commentsCard,
+    commonCard,
+  },
+  async created() {
+    try {
+      const postInfo = await Promise.all([
+        getPost(this.$route.params.postId),
+        getPostComments(this.$route.params.postId),
+      ]);
+      this.setCurrentPost({
+        postData: postInfo[0].data,
+        commnetsData: postInfo[1].data,
+      });
+      this.isLoader = false;
+    } catch (error) {
+      this.$router.push({ name: "404" });
+    }
+  },
+  unmounted() {
+    this.setCurrentPost({
+      postData: {},
+      commnetsData: {},
+    });
+  },
+  data() {
+    return {
+      isLoader: true,
+    };
+  },
+  methods: {
+    ...mapActions({ setCurrentPost: "setCurrentPost" }),
+  },
+  computed: {
+    ...mapGetters({ getCurrentPost: "getCurrentPost" }),
+  },
 };
 </script>
--- Vue Js Tasks/13-todo-app/src/router/index.ts
+++ Vue Js Tasks/13-todo-app/src/router/index.ts
@@ -1,50 +1,58 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-
-
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
 // pages
-import userLogin from  '@/pages/auth/user-login.page.vue'
-import viewPostsPage from  '@/pages/post/view-posts.page.vue'
-import viewSinglePostPage from '@/pages/post/view-single-post.page.vue'
-import pageNotFOund from '@/pages/common/page-not-found.page.vue'
-import addPost from  '@/pages/post/post-upsert.page.vue'
+import userLogin from "@/pages/auth/user-login.page.vue";
+import viewPostsPage from "@/pages/post/view-posts.page.vue";
+import viewSinglePostPage from "@/pages/post/view-single-post.page.vue";
+import pageNotFOund from "@/pages/common/page-not-found.page.vue";
+import addPost from "@/pages/post/post-upsert.page.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'home',
-    component: viewPostsPage
+    path: "/",
+    name: "home",
+    component: viewPostsPage,
   },
   {
-    path: '/auth',
-    name: 'login',
-    component: userLogin
+    path: "/posts",
+    name: "posts",
+    redirect: { name: "home" },
   },
   {
-    path: '/post/:postId',
-    name: 'post',
-    component: viewSinglePostPage
+    path: "/auth",
+    name: "login",
+    component: userLogin,
   },
   {
-    path: '/add-post',
-    name: 'addPost',
-    component: addPost
+    path: "/post/:postId",
+    name: "post",
+    component: viewSinglePostPage,
   },
   {
-    path: '/edit-post/:postId',
-    name: 'editPost',
-    component: addPost
+    path: "/add-post",
+    name: "addPost",
+    component: addPost,
+  },
+  {
+    path: "/edit-post/:postId",
+    name: "editPost",
+    component: addPost,
+  },
+  {
+    name: "pageNotFound",
+    path: "/:pageNotFound(.*)*",
+    redirect: { name: "404" },
   },
   {
     name: "404",
     path: "/404",
     component: pageNotFOund,
-},
-]
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
