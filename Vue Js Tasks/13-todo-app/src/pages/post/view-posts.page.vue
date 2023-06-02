9c8460012b4fa00a8d910ffd5b82b3dbc51480a2
khatrisagar <khatrisagar404@gmail.com>
2023-06-02 11:22:04 +0530

    added pagination in posts

--- Vue Js Tasks/13-todo-app/src/pages/post/view-posts.page.vue
+++ Vue Js Tasks/13-todo-app/src/pages/post/view-posts.page.vue
@@ -1,7 +1,19 @@
 <template>
+    <div class="pages-container">
+        <div v-for="page in getPages" :key="page">
+            <router-link :to="{ name: 'home', query: { page: page } }">{{
+                page
+            }}</router-link>
+        </div>
+    </div>
     <h1>------All Posts------</h1>
+    <br />
     <div class="card-container">
-        <commonCard v-for="post in getPosts" :key="post.id" :post="post" />
+        <commonCard
+            v-for="post in getPostForPage"
+            :key="post.id"
+            :post="post"
+        />
     </div>
 </template>
 
@@ -30,6 +42,31 @@ export default {
     },
     computed: {
         ...mapGetters({ getPosts: "post/getPosts" }),
+        getPostForPage() {
+            let posts = [];
+            for (let postIndex in this.getPosts) {
+                const page = parseInt(this.$route.query.page);
+                if (page) {
+                    if (postIndex >= (page - 1) * 10 && postIndex < page * 10) {
+                        posts.push(this.getPosts[postIndex]);
+                    }
+                } else {
+                    posts = this.getPosts;
+                }
+            }
+
+            return posts;
+        },
+        getPages() {
+            let pages = [];
+            const length = this.getPosts.length / 10;
+
+            for (let i = 1; i <= length; i++) {
+                pages.push(i);
+            }
+
+            return pages;
+        },
     },
 };
 </script>
@@ -39,5 +76,15 @@ export default {
     display: flex;
     flex-direction: column;
     gap: 1rem;
+}
+.pages-container {
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+    display: flex;
+    flex-direction: row;
+}
+.pages-container > div > a {
+    border: 1px solid black;
+    padding: 0.5rem;
 }
 </style>
